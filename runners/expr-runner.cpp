e23320c652ef2d2d70c8f12e2aef084ab796ef8b
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-02-12 22:01:54 +0530

    Minor fix to accessing parsed CLI

--- runners/expr-runner.cpp
+++ runners/expr-runner.cpp
@@ -90,7 +90,7 @@ int main(int argc, char *argv[]) {
 
   ScillaJIT::init();
 
-  auto InputFilename = boost::any_cast<std::string>(VM["input-file"].value());
+  auto InputFilename = VM["input-file"].as<std::string>();
   auto SJ = ExitOnErr(ScillaJIT::create(InputFilename));
   auto ScillaMainAddr = ExitOnErr(SJ->getAddressFor("scilla_main"));
   auto ScillaMain = reinterpret_cast<void (*)()>(ScillaMainAddr);
@@ -104,8 +104,7 @@ int main(int argc, char *argv[]) {
   }
 
   if (VM.count("output-file")) {
-    auto OutputFilename =
-        boost::any_cast<std::string>(VM["output-file"].value());
+    auto OutputFilename = VM["output-file"].as<std::string>();
     std::ofstream OFile(OutputFilename);
     if (!OFile) {
       std::cerr << "Error opening output file " << OutputFilename << "\n";
