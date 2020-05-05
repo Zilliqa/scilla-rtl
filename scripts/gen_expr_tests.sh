259314c45591921e2785e23c3b6f6b2720d6d3d5
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2020-05-05 20:42:20 +0530

    Update gen_expr_tests.sh script

--- scripts/gen_expr_tests.sh
+++ scripts/gen_expr_tests.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # Wrapper around gen_expr_test.sh that looks at all .ll files in a directory
-# and replaces it with a new one by running expr-compiler.
+# and replaces it with a new one by running expr-llvm.
 
 if [[ $# -ne 2 ]]
 then
@@ -10,20 +11,20 @@ fi
 scilla_root=$1
 testsuite_dir=$2
 
-if [[ ! -e ${scilla_root}/bin/expr-compiler || ! -d ${testsuite_dir}/expr ]]
+if [[ ! -e ${scilla_root}/bin/expr-llvm || ! -d ${testsuite_dir}/expr ]]
 then
-    echo "${scilla_root}/bin/expr-compiler or ${testsuite_dir}/expr not found"
+    echo "${scilla_root}/bin/expr-llvm or ${testsuite_dir}/expr not found"
     exit 1
 fi
 
 for llfile in ${testsuite_dir}/expr/*.ll
 do
-    llfile_base=`basename $llfile .ll`
+    llfile_base=$(basename $llfile .ll)
     inputf=${scilla_root}/tests/codegen/expr/${llfile_base}.scilexp
     if [[ ! -f ${scilla_root}/tests/codegen/expr/${llfile_base}.scilexp ]]
     then
         echo "$inputf input file not found"
     else
-        ./gen_expr_test.sh $inputf > $llfile
+        ./gen_expr_test.sh "$inputf" > "$llfile"
     fi
 done
