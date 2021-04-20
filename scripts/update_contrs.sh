a8d0d501161bb3b4065e43021e08073230b651c7
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-04-20 10:00:55 +0530

    Fix bug in test update scripts

--- scripts/update_contrs.sh
+++ scripts/update_contrs.sh
@@ -19,15 +19,18 @@ fi
 for llfile in ${testsuite_dir}/contr/*.ll
 do
     llfile_base=$(basename $llfile .ll)
-    input_f=${scilla_compiler_root}/tests/codegen/contr/gold/${llfile_base}.scilla.gold
-    input_debug_f=${scilla_compiler_root}/tests/codegen/contr/dgold/${llfile_base}.scilla.gold
-    input_source_f=${scilla_compiler_root}/tests/codegen/contr/${llfile_base}.scilla
-    if [[ ! -f $input_f || ! -f $input_debug_f || ! -f $input_source_f ]]
+    if [ "$(basename "$llfile_base" .dbg)" == "$llfile_base" ]
     then
-        echo "$input_f or $input_debug_f or $input_source_f input file(s) not found"
-    else
-        cat "$input_f" > "$llfile"
-        cat "$input_debug_f" > ${testsuite_dir}/contr/"${llfile_base}.dbg.ll"
-        scilla-checker -gaslimit 10000 -libdir "${scilla_compiler_root}"/src/stdlib -contractinfo "$input_source_f" > "${testsuite_dir}/contr/${llfile_base}.contrinfo.json"
+        input_f=${scilla_compiler_root}/tests/codegen/contr/gold/${llfile_base}.scilla.gold
+        input_debug_f=${scilla_compiler_root}/tests/codegen/contr/dgold/${llfile_base}.scilla.gold
+        input_source_f=${scilla_compiler_root}/tests/codegen/contr/${llfile_base}.scilla
+        if [[ ! -f $input_f || ! -f $input_debug_f || ! -f $input_source_f ]]
+        then
+            echo "$input_f or $input_debug_f or $input_source_f input file(s) not found"
+        else
+            cat "$input_f" > "$llfile"
+            cat "$input_debug_f" > ${testsuite_dir}/contr/"${llfile_base}.dbg.ll"
+            scilla-checker -gaslimit 10000 -libdir "${scilla_compiler_root}"/src/stdlib -contractinfo "$input_source_f" > "${testsuite_dir}/contr/${llfile_base}.contrinfo.json"
+        fi
     fi
 done
