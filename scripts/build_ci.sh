ec93448213a469330a3177e27fe08fc3f0bc9560
Vaivaswatha N <vaivaswatha@users.noreply.github.com>
2021-06-13 21:31:48 +0530

    Switch to GitHub Actions (#30)
    * Switch to GitHub Actions
    
    * GH Actions: Checkout submodules
    
    * Fix typo
    
    * CI: Give a name to the testsuite execution
    
    * Use 4 jobs to build
    
    * CI: Fix syntax error
    
    * Separate release and debug builds for CI
    
    * exec permission for dbuild_ci script

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-cmake -B build -S . -DADDRESS_SANITIZER=1
-cmake --build build --target runtests
+echo "Building release build with address sanitizer"
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
+cmake --build build --target runtests -j4
