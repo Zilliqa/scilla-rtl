e4407182f6d2b715b1033b4019469df6ced0455d
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-06-13 21:18:29 +0530

    Separate release and debug builds for CI

---
+++ scripts/dbuild_ci.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Building debug build with address sanitizer"
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1
+cmake --build build --target runtests -j4
