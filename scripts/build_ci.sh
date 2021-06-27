9ad919638294981f2e35e5febbab2084b14270d1
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-06-27 21:46:13 +0530

    Rename runtests build target to the more standard check

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -2,4 +2,4 @@
 
 echo "Building release build with address sanitizer"
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
-cmake --build build --target runtests -j4
+cmake --build build --target check -j4
--- scripts/dbuild_ci.sh
+++ scripts/dbuild_ci.sh
@@ -2,4 +2,4 @@
 
 echo "Building debug build with address sanitizer"
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1
-cmake --build build --target runtests -j4
+cmake --build build --target check -j4
