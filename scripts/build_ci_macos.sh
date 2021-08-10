a4f79c61b6c623c5ed00854816aca43a9a33315c
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2021-08-10 15:07:36 +0800

    Rearranging again

--- scripts/build_ci_macos.sh
+++ scripts/build_ci_macos.sh
@@ -2,5 +2,6 @@
 
 echo "Building release build with address sanitizer"
 export PATH=/usr/local/opt/llvm/bin:$PATH 
+export LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ 
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
-LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ cmake --build build --target runtests -j4
+cmake --build build --target runtests -j4
