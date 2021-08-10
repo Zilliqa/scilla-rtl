e0a77559e2aea22ac8d6a4ecb5a5d44c46b3e3e9
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2021-08-10 13:49:25 +0800

    export path for clang

--- scripts/build_ci_macos.sh
+++ scripts/build_ci_macos.sh
@@ -2,4 +2,5 @@
 
 echo "Building release build with address sanitizer"
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
+export PATH=/usr/local/opt/llvm/bin:$PATH 
 LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ cmake --build build --target runtests -j4
