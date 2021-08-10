9b294b1978b7d496ba19c136cb0791f1efb99552
Tram Hoang <tram.hoang@u.yale-nus.edu.sg>
2021-08-10 15:31:05 +0800

    Dbuild mac support

---
+++ scripts/dbuild_ci_macos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Building debug build with address sanitizer"
+export PATH=/usr/local/opt/llvm/bin:$PATH 
+export LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ 
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1
+cmake --build build --target runtests -j4
