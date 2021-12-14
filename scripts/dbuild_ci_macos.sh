f14cc095fa751c93ba58fe12bd0acd9b7b67ec7b
Vaivaswatha Nagaraj <vaivaswatha@zilliqa.com>
2021-12-14 13:23:30 +0530

    MacOS CI debug script to LLVM@13

--- scripts/dbuild_ci_macos.sh
+++ scripts/dbuild_ci_macos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Building debug build with address sanitizer"
-export PATH=/usr/local/opt/llvm@12/bin:$PATH 
+export PATH=/usr/local/opt/llvm@13/bin:$PATH 
 export LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ 
 cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1
 cmake --build build --target runtests -j4
