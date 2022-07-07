37c2a0314c063de3522f283ed2b0f069635164b2
Anton Trunov <anton@zilliqa.com>
2022-07-07 14:42:50 +0300

    Fix 'implicit instantiation of undefined template' error
    Here is the error:
    
    ```
    /Users/anton/zilliqa/scilla-rtl/testsuite/ContrTests.cpp:147:23: error: implicit instantiation of undefined template 'std::basic_ofstream<char>'
            std::ofstream Out(PathPrefix + Input.ExpectedStateFilename);
                          ^
    /usr/local/opt/llvm@13/bin/../include/c++/v1/iosfwd:149:32: note: template is declared here
        class _LIBCPP_TEMPLATE_VIS basic_ofstream;
                                   ^
    /Users/anton/zilliqa/scilla-rtl/testsuite/ContrTests.cpp:162:23: error: implicit instantiation of undefined template 'std::basic_ofstream<char>'
            std::ofstream Out(PathPrefix + Input.ExpectedOutputFilename);
                          ^
    /usr/local/opt/llvm@13/bin/../include/c++/v1/iosfwd:149:32: note: template is declared here
        class _LIBCPP_TEMPLATE_VIS basic_ofstream;
                                   ^
    2 errors generated.
    make[3]: *** [testsuite/CMakeFiles/scilla-testsuite.dir/ContrTests.cpp.o] Error 1
    make[3]: *** Waiting for unfinished jobs....
    make[2]: *** [testsuite/CMakeFiles/scilla-testsuite.dir/all] Error 2
    make[1]: *** [testsuite/CMakeFiles/runtests.dir/rule] Error 2
    make: *** [runtests] Error 2
    ```

--- testsuite/ContrTests.cpp
+++ testsuite/ContrTests.cpp
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <fstream>
 #include <boost/filesystem.hpp>
 #include <boost/test/tools/output_test_stream.hpp>
 #include <boost/test/unit_test.hpp>
