b75b090102509175b9aab3b5afb614c6b6ca33cd
Anton Trunov <anton@zilliqa.com>
2022-07-07 13:46:14 +0300

    Make macOS CI script pick the right Clang version

--- scripts/build_ci_macos.sh
+++ scripts/build_ci_macos.sh
@@ -3,5 +3,12 @@
 echo "Building release build with address sanitizer"
 export PATH=/usr/local/opt/llvm@13/bin:$PATH
 export LIBRARY_PATH=/usr/local/opt/jsoncpp/lib:/usr/local/opt/secp256k1/lib/ 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
+
+# CMAKE_C_COMPILER and CMAKE_CXX_COMPILER make cmake pick the Homebrew Clang installation
+# and not the default AppleClang compiler
+# See https://github.com/taichi-dev/taichi/issues/3578 and some links there:
+# - https://github.com/taichi-dev/taichi/pull/3379
+# - https://stackoverflow.com/questions/45933732/how-to-specify-a-compiler-in-cmake
+cmake -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -B build -S . -DCMAKE_BUILD_TYPE=Release -DADDRESS_SANITIZER=1
+
 cmake --build build --target runtests -j4
